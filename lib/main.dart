966967b7489c1889e2c13e6ca36cf3582c95d91f
Oswi <edwin.roman@ucb.edu.bo>
2024-04-30 09:45:17 -0400

    Se cambio el examen según la documentación de firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:moviles_examen2/firebase_options.dart';
-import 'package:moviles_examen2/notification_screen.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,11 +16,23 @@ void main() async {
   );
   requestNotificationPermission();
   await FirebaseMessaging.instance.setAutoInitEnabled(true);
-  runApp(MyApp());
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print('TOKEN data: $fcmToken');
+  runApp(const MyApp());
 }
 
 Future<void> requestNotificationPermission() async {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
+
   NotificationSettings settings = await messaging.requestPermission(
     alert: true,
     announcement: false,
@@ -25,14 +42,26 @@ Future<void> requestNotificationPermission() async {
     provisional: false,
     sound: true,
   );
-  print('Notification permission granted: ${settings.authorizationStatus}');
+
+  print('User granted permission: ${settings.authorizationStatus}');
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: NotificationScreen(),
+      title: 'Examen2',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Notificaciones'),
+        ),
+      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:moviles_examen2/notification_screen.dart';
+import 'package:moviles_examen2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(NotificationScreen());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
